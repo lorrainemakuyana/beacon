c624a400c3e1feee3bf87a4acf4ea634596d919a
lorrainemakuyana <lorrainem.dev@gmail.com>
2026-04-27 22:16:52 +0100

    chore(components): remove unused components from the mobile directory

--- mobile/components/auth-header.tsx
+++
@@ -1,6 +0,0 @@
-import { Image } from "react-native";
-import Logo from "@/assets/images/logo.png";
-
-export default function AuthHeader() {
-  return <Image style={{ width: 50, height: 50 }} source={Logo} />;
-}
